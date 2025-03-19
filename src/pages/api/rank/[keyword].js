92df4d028a232bea159547706c519954d052d222
heroyooi <heroyooi1018@gmail.com>
2025-03-20 08:53:13 +0900

    CORS 허용

--- src/pages/api/rank/[keyword].js
+++ src/pages/api/rank/[keyword].js
@@ -1,6 +1,10 @@
 import puppeteer from 'puppeteer';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'GET 요청만 허용됩니다.' });
   }
