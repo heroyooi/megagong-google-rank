800779dd3f3a305748d7b41e828711986294ad3f
heroyooi <heroyooi1018@gmail.com>
2025-03-20 22:45:19 +0900

    gpu 실행 추가

--- src/pages/api/rank/[keyword].js
+++ src/pages/api/rank/[keyword].js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-blink-features=AutomationControlled',
+        '--disable-gpu',
       ],
     });
 
